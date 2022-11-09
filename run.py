dfd0996c20b1ffee327b1336487cf415e045303d
UIT_20521888 <20521888@gm.uit.edu.vn>
2022-11-09 11:50:16 +0700

    fix delete file

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 from scipy.stats import ttest_ind
 from draw_graph import draw_grap
+import shutil
 
 MSSV = 20521888
 LIST_POP = [32, 64, 128, 256, 512, 1024]
@@ -16,13 +17,13 @@ def run_for_object_function(object_function: list  = LIST_FUNCTIONS):
     path_log = './File_log'
     path_result = './File_result'
     if os.path.exists(path_gif) == True:
-        os.rmdir(path_gif)
+        shutil.rmtree(path_gif)
     if os.path.exists(path_images) == True:
-        os.rmdir(path_images)
+        shutil.rmtree(path_images)
     if os.path.exists(path_log) == True:
-        os.rmdir(path_log)
+        shutil.rmtree(path_log)
     if os.path.exists(path_result) == True:
-        os.rmdir(path_result)
+        shutil.rmtree(path_result)
     os.mkdir(path_gif)
     os.mkdir(path_images)
     os.mkdir(path_log)
