fix overflow RAM

algorithm.py
@@ -2,6 +2,7 @@ from utils import sphere, zakharov, rosenbrock, michalewicz, ackley, clip_number
 import numpy as np
 from animation import draw_graph3D
 import os
+import pandas as pd
 
 class DE:
     def __init__(self, num_individuals: int = 32,
@@ -112,14 +113,20 @@ class DE:
         f.close()
         # print(history_pop.shape)
         if self.num_parameters == 2 and self.num_individuals == 32 and self.random_seed == 20521888:
-            file_name = os.path.join(path_file_gif,f"DE_{self.objective_function}_{self.num_parameters}")
-            os.mkdir(file_name)
-            file_name_next = os.path.join(file_name,f"DE_{self.objective_function}_{self.num_parameters}")
-            draw_graph3D(xdata = history_pop[:,:,0] ,
-                 ydata = history_pop[:,:,1],
-                 zdata = history_fitness,
-                 objective_function = self.objective_function,
-                 filename = file_name_next)
+            file_name = os.path.join(path_file_gif,f"DE_{self.objective_function}_{self.num_parameters}.xlsx")
+            dic = {"X_data":history_pop[:,:,0],
+                    "Y_data": history_pop[:,:,0],
+                    "Fitness":history_fitness}
+            data_df = pd.DataFrame(dic)
+              #        print(dic)
+            data_df.to_excel(file_name, index = False)
+            # os.mkdir(file_name)
+            # file_name_next = os.path.join(file_name,f"DE_{self.objective_function}_{self.num_parameters}")
+            # draw_graph3D(xdata = history_pop[:,:,0] ,
+            #      ydata = history_pop[:,:,1],
+            #      zdata = history_fitness,
+            #      objective_function = self.objective_function,
+            #      filename = file_name_next)
         
         return history_best_eval
 
@@ -237,14 +244,20 @@ class CEM:
 
         history_pop, history_fitness = np.array(history_pop), np.array(history_fitness)
         if self.num_parameters == 2 and self.num_individuals == 32 and self.random_seed == 20521888:
-            file_name = os.path.join(path_file_gif,f"CEM_{self.objective_function}_{self.num_parameters}")
-            os.mkdir(file_name)
-            file_name_next = os.path.join(file_name,f"CEM_{self.objective_function}_{self.num_parameters}")
-            draw_graph3D(xdata = history_pop[:,:,0] ,
-                 ydata = history_pop[:,:,1],
-                 zdata = history_fitness,
-                 objective_function = self.objective_function,
-                 filename = file_name_next)
+            file_name = os.path.join(path_file_gif,f"CEM_{self.objective_function}_{self.num_parameters}.xlsx")
+            # os.mkdir(file_name)
+            # file_name_next = os.path.join(file_name,f"CEM_{self.objective_function}_{self.num_parameters}")
+            # draw_graph3D(xdata = history_pop[:,:,0] ,
+            #      ydata = history_pop[:,:,1],
+            #      zdata = history_fitness,
+            #      objective_function = self.objective_function,
+            #      filename = file_name_next)
+            dic = {"X_data":history_pop[:,:,0],
+                    "Y_data": history_pop[:,:,0],
+                    "Fitness":history_fitness}
+            data_df = pd.DataFrame(dic)
+    #        print(dic)
+            data_df.to_excel(file_name, index = False)
         
         return history_best_eval
 